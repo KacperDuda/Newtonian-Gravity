895b945b537eb2a6e27b86856853d287e2066b41
Kacper Duda <kacperduda2002@gmail.com>
2023-09-09 21:55:26 +0200

    Improving the way a user can choose whether they want to plot or save to file

--- data_generation/main.py
+++ data_generation/main.py
@@ -1,20 +1,36 @@
 from planet import *
 from typing import List
 import simulator
+import math
 import matplotlib.pyplot as plt
 import json
 
 G = 6.31654418845696  # gravity constant in our model
-computational_delta = 0.000001  # amount of time between frames in seconds
-timeToSimulate = 10  # in seconds
-show_delta = 0.1
+computational_delta = 0.00001  # amount of time between frames in seconds
+timeToSimulate = 3  # in seconds
+show_delta = 0.01  # showing how often user should have data
+
+to_plot = True
+filename = "4-body-disaster"
 
 
 def main():
+    # planets: List[Planet] = [
+    #     Planet(1, 1, 0, 0, 0.4 * math.pi),
+    #     Planet(1, -1, 0, 0, -0.4 * math.pi)
+    # ]
+
+    # planets: List[Planet] = [
+    #     Planet(1, 1, 1, 0, 0),
+    #     Planet(1, -1, 0, 0, 0),
+    #     Planet(1, 1.5, -0.5, 0, 0)
+    # ]
+
     planets: List[Planet] = [
-        Planet(1, 1, 0, 0, 1.26),
-        Planet(1, -1, 0, 0, -1.26),
-        # Planet(3, 3, 3, 0, 0)
+        Planet(1, 1, 1, 0, 0),
+        Planet(1, -1, 0, 0, 0),
+        Planet(3, 0, 0, 0, 0),
+        Planet(1, 1.5, -0.5, 0, 0)
     ]
 
     # simulating
@@ -22,40 +38,49 @@ def main():
         simulator.next_frame(planets, computational_delta)
 
     # plotting
-    # for planet in planets:
-    #     x = []
-    #     y = []
-    #     for idx, pos in enumerate(planet.positions):
-    #         if idx % int(show_delta / computational_delta) == 0:
-    #             x.append(pos.x)
-    #             y.append(pos.y)
-    #     plt.scatter(x, y, s=1)
-    #
-    # plt.axis('square')
-    # plt.show()
+    if to_plot:
+        for planet in planets:
+            x = []
+            y = []
+            for idx, pos in enumerate(planet.positions):
+                if idx % int(show_delta / computational_delta) == 0:
+                    x.append(pos.x)
+                    y.append(pos.y)
+            plt.scatter(x, y, s=1)
 
-    output = {
-        "amount": len(planets),
-        "delta": show_delta,
-        "planets": []
-    }
+        plt.axis('square')
+        plt.show()
 
-    for planet in planets:
-        shown_positions = []
-        for idx, pos in enumerate(planet.positions):
-            if idx % int(show_delta / computational_delta) == 0:
-                shown_positions.append({
-                    "x": pos.x,
-                    "y": pos.y
-                })
+    else:
+        output = {
+            "amount": len(planets),
+            "frames": 0,
+            "delta": show_delta,
+            "planets": []
+        }
 
-        output["planets"].append({
-            "mass": planet.m,
-            "positions": shown_positions
-        })
+        frame_o_meter = 0
 
-    with open("../data/sample.json", "w") as outfile:
-        json.dump(output, outfile)
+        # saving to a file
+        for planet in planets:
+            shown_positions = []
+            for idx, pos in enumerate(planet.positions):
+                if idx % int(show_delta / computational_delta) == 0:
+                    shown_positions.append({
+                        "x": pos.x,
+                        "y": pos.y
+                    })
+                    frame_o_meter += 1
+
+            output["planets"].append({
+                "mass": planet.m,
+                "positions": shown_positions
+            })
+
+        output["frames"] = frame_o_meter
+
+        with open(f"../data/{filename}.json", "w") as outfile:
+            json.dump(output, outfile)
 
 
 if __name__ == '__main__':
