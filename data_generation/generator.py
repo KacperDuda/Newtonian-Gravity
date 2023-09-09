055dce26f3e03cfd0bf756ad28da16346dd9733c
Kacper Duda <kacperduda2002@gmail.com>
2023-09-09 22:54:02 +0200

    Improving generator input

--- data_generation/generator.py
+++ data_generation/generator.py
@@ -3,5 +3,14 @@ from simulator import next_frame
 
 
 # generating a give amount of
-def generate(planet_amount, iterations):
-    return None
+def generate(planet_amount: int, iterations: int, frames: int, filename: str) -> None:
+    results = []
+    for i in range(iterations):
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("Generator main.")
