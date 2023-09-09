f04ea8300febec2aa36257f236d844dedd38ca0e
Kacper Duda <kacperduda2002@gmail.com>
2023-09-09 21:58:30 +0200

    Testing keras instalation

---
+++ model/main.py
@@ -0,0 +1,21 @@
+
+import tensorflow
+from tensorflow.keras.models import Sequential # no feedback loop
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+
+print("Num GPUs Available: ", len(tensorflow.config.experimental.list_physical_devices('GPU')))
+
+model = Sequential()
+
+model.add(Dense(32, input_dim=13, activation='relu'))
+model.add(BatchNormalization())
+
+model.add(Dense(32, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.4))
+
+model.add(Dense(16, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+model.summary()
