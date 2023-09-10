5a762d62c32899d4d13057d587f5c33e2870bf5a
Kacper Duda <kacperduda2002@gmail.com>
2023-09-10 11:29:25 +0200

    User input in generator

--- data_generation/generator.py
+++ data_generation/generator.py
@@ -1,16 +1,25 @@
 from planet import *
 from simulator import next_frame
+import json
 
 
-# generating a give amount of
-def generate(planet_amount: int, iterations: int, frames: int, filename: str) -> None:
+# generating a give amount of iteration for machine learning and saves it inside files
+def generate(planet_amount: int, iterations: int, output_frames: int, delta, filename: str) -> None:
     results = []
     for i in range(iterations):
-
+        print(i)
 
 
 
 
 
 if __name__ == '__main__':
-    print("Generator main.")
+    p_amount =  int(input("Planet Amount: ") or 3)
+    iter = int(input("Iterations: ") or 15)
+    frames = int(input("Output rrames per iteration") or 20)
+    delta = int(input("Time in ms between all frames") or 100)
+    filename = input("filename") or 'test'
+
+    generate(p_amount, iter, frames, delta, filename)
+
+
