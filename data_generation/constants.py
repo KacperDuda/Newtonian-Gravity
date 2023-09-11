9772a7d3a71242bf2f431d1d7f825a240f170ed6
Kacper Duda <kacperduda2002@gmail.com>
2023-09-11 13:53:07 +0200

    Splitting constants to separated file

---
+++ data_generation/constants.py
@@ -0,0 +1,6 @@
+# CONSTANTS
+G = 6.31654418845696  # gravity constant in our model
+computational_delta = 0.00001  # amount of time between frames in seconds
+timeToSimulate = 50  # in seconds
+show_delta = 0.01  # showing how often user should have data
+round_to = 5  # amount of places after decimal
