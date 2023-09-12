c6d564f20986cdf9136b1b0407ca22f364ce1550
amlynczak <95677220+amlynczak@users.noreply.github.com>
2023-09-12 16:11:29 +0200

    added commented parts to help building it later

--- model/main.py
+++ model/main.py
@@ -6,6 +6,9 @@ from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 # nothing is ready
 # print("Num GPUs Available: ", len(tensorflow.config.experimental.list_physical_devices('GPU'))
 
+#loading data
+#(X_train, y_train), (X_valid, y_valid) =
+
 
 model = Sequential()
 
@@ -21,3 +24,7 @@ model.add(Dense(16, activation='relu'))
 model.add(Dropout(0.1))
 
 model.summary()
+
+#model.compile(loss = ??, optimizer = ??, metrics= ??)
+
+#model.fit(X_train, y_train, batch_size = 128, epoch = 10, verbose = 1, validation_data = (X_valid, y_valid))
