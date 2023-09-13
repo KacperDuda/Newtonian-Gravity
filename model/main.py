ead0172b8f5f3626645f73597b8604b71e4e9ab3
Kacper Duda <kacperduda2002@gmail.com>
2023-09-13 19:02:36 +0200

    Defining model structure

--- model/main.py
+++ model/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 
 import datetime
-time = datetime.datetime.now().strftime("%Y.%m.%d.%H.%M.%S")
+time = datetime.datetime.now().strftime("%m.%d.%H.%M")
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # 0 - all messages, 1 - no INFO, 2 - no warning, 3 - no error reporting
 
@@ -16,6 +16,7 @@ Sequential = tf.keras.models.Sequential
 Dense = tf.keras.layers.Dense
 Dropout = tf.keras.layers.Dropout
 BatchNormalization = tf.keras.layers.BatchNormalization
+load = tf.keras.models.load_model
 
 
 def newSection():
@@ -25,13 +26,13 @@ def newSection():
 newSection()
 print("Number of GPUs to use: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-# # impoerting data
-with open("../data/10k80ms2s.json") as trainingSet:
+# impoerting data
+with open("../data/experiment_one/10k80ms2s.json") as trainingSet:
     data_train = json.load(trainingSet)
     X_train = np.array(data_train["X"])
     Y_train = np.array(data_train["y"])
 
-with open("../data/2k80ms2s.json") as validatingSet:
+with open("../data/experiment_one/3p1000i80f25.0d.json") as validatingSet:
     data_train = json.load(validatingSet)
     X_val = np.array(data_train["X"])
     Y_val = np.array(data_train["y"])
@@ -49,23 +50,35 @@ print(f"Model input is {X_train.shape[1]} neurons and output is {Y_train.shape[1
 print(f"Training size: {X_train.shape[0]}, validation size: {Y_train.shape[0]}")
 
 newSection()
+saved_model = input("Podaj nazwę modelu (brak): ")
+if saved_model:
+    model = load(f'../model_data/{saved_model}.h5')
+    print("Model loaded successfully from file")
+else:
+    model = Sequential()
+    # input layer
+    model.add(Dense(16, input_shape=(16,), activation="linear"))
 
-model = Sequential()
-# input layer
-model.add(Dense(16, input_shape=(16,), activation="linear"))
 
-for i in range(5):
-    model.add(Dense(500 + (50*i), activation="linear"))
-    model.add(BatchNormalization())
+    model.add(Dense(500, activation="relu"))
     model.add(Dropout(0.25))
 
-model.add(Dense(474, activation="linear"))
+    model.add(Dense(500, activation="relu"))
+    model.add(Dropout(0.35))
 
-model.summary()
-model.compile(optimizer="adam", loss="mean_squared_error")
+    model.add(Dense(500, activation="relu"))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(750, activation="relu"))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(480, activation="linear"))
+
+    model.summary()
+    model.compile(optimizer="adam", loss="mean_squared_error")
 newSection()
 
 # TRAINING
-for i in range(100):
+for i in range(int(input("Ilość epok * 1000: "))):
     model.fit(X_train, Y_train, epochs=1000, batch_size=1000, validation_data=(X_val, Y_val))
     model.save(f"{os.path.dirname(os.path.dirname(__file__))}/model_data/{time}/{i}.h5")
